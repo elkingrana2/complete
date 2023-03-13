5d8fc836501c513bdb5e3ee4ad7022be798e31d9
elkingrana2 <elkingc1@gmail.com>
2023-03-13 15:56:08 -0500

    Indicadores listo

---
+++ src/main/java/com/example/parqueaderoapi/controllers/CorreoController.java
@@ -0,0 +1,11 @@
+package com.example.parqueaderoapi.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("correoApi/correo")
+public class CorreoController {
+
+  // private final CorreoService correoService;
+
+}
